ba05c6162f2cbe9d3bfacff421ce1ff930162872
sayamarber <dany.berumen@gmail.com>
2020-06-06 00:54:20 -0500

    ruteo terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { FormacionComponent } from './formacion/formacion.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { QuienSoyComponent } from './quien-soy/quien-soy.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'quien-soy', pathMatch: 'full' },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'experiencia', component: ExperienciaComponent },
+  { path: 'formacion', component: FormacionComponent },
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: 'quien-soy', component: QuienSoyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { QuienSoyComponent } from './quien-soy/quien-soy.component';
+import { FormacionComponent } from './formacion/formacion.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    QuienSoyComponent,
+    FormacionComponent,
+    ProyectosComponent,
+    ExperienciaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
